b6b5933a1b7bc2e2e0cff2cb133201783c01034e
daryl <daryl.mcafee@gmail.com>
2023-08-10 09:30:03 -0400

    Added a button to iterate upwards through pokedex numbers.

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+let number = 1;
+const pokeImg = document.querySelector("#Pokemon");
+const addBtn = document.querySelector("#addBtn");
+
 const fetchPromise = fetch( 
-    "https://pokeapi.co/api/v2/pokemon/dragonite",
+    "https://pokeapi.co/api/v2/pokemon/" + number, // using a variable to represent the Pokedex number
 );
-console.log(fetchPromise);
+// Make sure to use pokemon and not pokemon-species
 
 fetchPromise.then((response) => response.json()) 
 // Converts the response from the server to usable data 
 .then((data) => { 
-    const pokeImg = document.querySelector("#Pokemon")
     console.log(data);
     pokeImg.src = data.sprites.front_default;
     console.log(data.sprites);
 })
-.catch((err) => { 
-    console.log("NO PKMN")
+.catch((err) => { // Only happens if there is a network error.
+    console.log("NO PKMN");
 })
+
+function newPokemon(){
+    number ++
+    return fetch(
+        "https://pokeapi.co/api/v2/pokemon/" + number,
+        //start of a function to refetch a new Pokemon at another dex number.
+    )
+}
+
+function imgUpdate(){
+    newPokemon().then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        pokeImg.src = data.sprites.front_default;
+    })  
+}
+
+addBtn.addEventListener("click", imgUpdate);
+
+
